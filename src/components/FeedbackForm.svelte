a65d71753fe8a50fb650a85c12e13bc8ad58eb8b
rohank789 <rohank@noitavonne.com>
2022-08-07 16:56:49 +0530

    initail commit

---
+++ src/components/FeedbackForm.svelte
@@ -0,0 +1,97 @@
+<script>
+  import { v4 as uuidv4 } from "uuid";
+  import { FeedbackStore } from "../store.js";
+  import Button from "./Button.svelte";
+  import Card from "./Card.svelte";
+  import RatingSelect from "./RatingSelect.svelte";
+
+  let text = "";
+  let rating = 10;
+  let btnDisable = true;
+  let min = 10;
+  let message;
+
+  const handleInput = () => {
+    if (text.trim().length <= min) {
+      message = `Text must be at least ${min} characters`;
+      btnDisable = true;
+    } else {
+      message = null;
+      btnDisable = false;
+    }
+  };
+
+  const handleSelect = (e) => (rating = e.detail);
+
+  const handleSubmit = () => {
+    if (text.trim().length > min) {
+      const newFeedback = {
+        id: uuidv4(),
+        text,
+        rating: +rating,
+      };
+
+      // $FeedbackStore = [newFeedback, ...$FeedbackStore];
+      FeedbackStore.update((currentFeedback) => {
+        return [newFeedback, ...currentFeedback];
+      });
+      text = "";
+    }
+  };
+</script>
+
+<Card>
+  <header>
+    <h2>How would you rate your service with us?</h2>
+  </header>
+
+  <form on:submit|preventDefault={handleSubmit}>
+    <RatingSelect {rating} on:rating-select={handleSelect} />
+    <div class="input-group">
+      <input
+        type="text"
+        bind:value={text}
+        on:input={handleInput}
+        placeholder="Tell us something that keeps you coming back"
+      />
+      <Button type="submit" disabled={btnDisable}>Send</Button>
+    </div>
+    {#if message}
+      <div class="message">
+        {message}
+      </div>
+    {/if}
+  </form>
+</Card>
+
+<style>
+  header {
+    max-width: 400px;
+    margin: auto;
+  }
+
+  header h2 {
+    font-size: 22px;
+    font-weight: 600;
+    text-align: center;
+  }
+
+  .input-group {
+    display: flex;
+    flex-direction: row;
+    border: 1px solid #ccc;
+    padding: 8px 10px;
+    border-radius: 8px;
+    margin-top: 15px;
+  }
+
+  input {
+    flex-grow: 2;
+    border: none;
+    font-size: 16px;
+  }
+
+  input:focus {
+    outline: none;
+  }
+</style>
